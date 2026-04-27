4e0d095fe55c1be1ca95dff3698b959c95da2a58
jsmalc-algebra <jsmalc2@algebra.hr>
2026-04-27 03:14:32 +0200

    Begging to add handling of submitted reservations

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\ReservationsType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ReservationController extends AbstractController
 {
     #[Route('/reservations', name: 'reservation_form')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
-        $form = $this->createForm(ReservationsType::class);
+        $reservation = new Reservation();
+
+        $form = $this->createForm(ReservationsType::class, $reservation);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($reservation);
+
+            $manager->flush();
+
+            dd($reservation);
+        }
 
         return $this->render('reservation/index.html.twig', [
             'controller_name' => 'ReservationController',
